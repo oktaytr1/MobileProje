26df1310d60da69bef80899d4af2f77cccbaf61c
oktaytr1 <ahmetmuhammet719@gmail.com>
2024-12-09 04:39:24 +0300

    yeni ekleme

---
+++ Barbershop/Models/Barber.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Barbershop.Models
+{
+    public class Barber
+    {
+
+        public int BarberId { get; set; }
+
+        public string? BarberName { get; set; }
+
+        // child ref
+        public List<Appointment>? Appointments { get; set; }
+       // public Appointment Appointment { get; set; }
+    }
+}
